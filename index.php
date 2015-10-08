75edb99a834978768f2403d863800d0915c2b5ac
annamarialarsson <annamaria@Annas-MacBook-Pro.local>
2015-10-08 17:06:51 +0200

    Fixa DIY sida med bildgrid

--- index.php
+++ index.php
@@ -1,8 +1,12 @@
 <?php get_header(); ?>
 
-<p class="l-gutter"><img src="<?php echo get_template_directory_uri(); ?>/demo_images/diy1.jpg"></p>
-<p class="l-gutter"><img src="<?php echo get_template_directory_uri(); ?>/demo_images/diy2.jpg"></p>
+<div class="o-preview-diy l-gutter">
+	<div class="a-diy-imageholder" style="background-image: url(<?php echo get_template_directory_uri(); ?>/demo_images/diy1.jpg)"></div>
+	<h3 class="a-small">DIY - Monthly Makers roliga temat RYMDEN</h3>
+	<p class="m-diy-fineprint">22 / 09 / 2015  |  2 kommentarer</p>
+</div>
+<!-- <p class="l-gutter"><img src="<?php echo get_template_directory_uri(); ?>/demo_images/diy2.jpg"></p>
 <p class="l-gutter"><img src="<?php echo get_template_directory_uri(); ?>/demo_images/diy3.jpg"></p>
-<p class="l-gutter"><img src="<?php echo get_template_directory_uri(); ?>/demo_images/diy4.jpg"></p>
+<p class="l-gutter"><img src="<?php echo get_template_directory_uri(); ?>/demo_images/diy4.jpg"></p> -->
 
 <?php get_footer(); ?>
