a7c8ea80a2da66cc0902a6bd60427ae2b348d7ae
annamarialarsson <annamaria@Annas-MacBook-Pro.local>
2015-10-16 10:41:38 +0200

    Bildfix listningsvy

--- index.php
+++ index.php
@@ -1,12 +1,71 @@
 <?php get_header(); ?>
 
-<div class="o-preview-diy l-gutter">
-	<div class="a-diy-imageholder" style="background-image: url(<?php echo get_template_directory_uri(); ?>/demo_images/diy1.jpg)"></div>
-	<h3 class="a-small">DIY - Monthly Makers roliga temat RYMDEN</h3>
-	<p class="m-diy-fineprint">22 / 09 / 2015  |  2 kommentarer</p>
+<div class="m-preview-diy l-gutter">
+	<img src="<?php bloginfo( 'stylesheet_directory' ); ?>/demo_images/diy1.jpg" alt="Monthly Makers rynden t-shirt">
+	<h3 class="a-small">DIY - Monthly Makers RYMDEN</h3>
+	<p class="a-fineprint">22 / 09 / 2015  |  2 kommentarer</p>
 </div>
-<!-- <p class="l-gutter"><img src="<?php echo get_template_directory_uri(); ?>/demo_images/diy2.jpg"></p>
-<p class="l-gutter"><img src="<?php echo get_template_directory_uri(); ?>/demo_images/diy3.jpg"></p>
-<p class="l-gutter"><img src="<?php echo get_template_directory_uri(); ?>/demo_images/diy4.jpg"></p> -->
 
+<div class="m-preview-diy l-gutter">
+	<img src="<?php bloginfo( 'stylesheet_directory' ); ?>/demo_images/diy2.jpg" alt="Monthly Makers rynden t-shirt">
+	<h3 class="a-small">DIY - Cut off jeans</h3>
+	<p class="a-fineprint">22 / 09 / 2015  |  2 kommentarer</p>
+</div>
+
+<div class="m-preview-diy l-gutter">
+	<img src="<?php bloginfo( 'stylesheet_directory' ); ?>/demo_images/diy3.jpg" alt="Monthly Makers rynden t-shirt">
+	<h3 class="a-small">DIY - Marmorerad muggar i BOBO nr3</h3>
+	<p class="a-fineprint">22 / 09 / 2015  |  2 kommentarer</p>
+</div>
+
+<div class="m-preview-diy l-gutter">
+	<img src="<?php bloginfo( 'stylesheet_directory' ); ?>/demo_images/diy4.jpg" alt="Monthly Makers rynden t-shirt">
+	<h3 class="a-small">DIY - Soffbord i guld och marmor</h3>
+	<p class="a-fineprint">22 / 09 / 2015  |  2 kommentarer</p>
+</div>
+<div class="m-preview-diy l-gutter">
+	<img src="<?php bloginfo( 'stylesheet_directory' ); ?>/demo_images/diy1.jpg" alt="Monthly Makers rynden t-shirt">
+	<h3 class="a-small">DIY - Monthly Makers RYMDEN</h3>
+	<p class="a-fineprint">22 / 09 / 2015  |  2 kommentarer</p>
+</div>
+
+<div class="m-preview-diy l-gutter">
+	<img src="<?php bloginfo( 'stylesheet_directory' ); ?>/demo_images/diy2.jpg" alt="Monthly Makers rynden t-shirt">
+	<h3 class="a-small">DIY - Cut off jeans</h3>
+	<p class="a-fineprint">22 / 09 / 2015  |  2 kommentarer</p>
+</div>
+
+<div class="m-preview-diy l-gutter">
+	<img src="<?php bloginfo( 'stylesheet_directory' ); ?>/demo_images/diy3.jpg" alt="Monthly Makers rynden t-shirt">
+	<h3 class="a-small">DIY - Marmorerad muggar i BOBO nr3</h3>
+	<p class="a-fineprint">22 / 09 / 2015  |  2 kommentarer</p>
+</div>
+
+<div class="m-preview-diy l-gutter">
+	<img src="<?php bloginfo( 'stylesheet_directory' ); ?>/demo_images/diy4.jpg" alt="Monthly Makers rynden t-shirt">
+	<h3 class="a-small">DIY - Soffbord i guld och marmor</h3>
+	<p class="a-fineprint">22 / 09 / 2015  |  2 kommentarer</p>
+</div><div class="m-preview-diy l-gutter">
+	<img src="<?php bloginfo( 'stylesheet_directory' ); ?>/demo_images/diy1.jpg" alt="Monthly Makers rynden t-shirt">
+	<h3 class="a-small">DIY - Monthly Makers RYMDEN</h3>
+	<p class="a-fineprint">22 / 09 / 2015  |  2 kommentarer</p>
+</div>
+
+<div class="m-preview-diy l-gutter">
+	<img src="<?php bloginfo( 'stylesheet_directory' ); ?>/demo_images/diy2.jpg" alt="Monthly Makers rynden t-shirt">
+	<h3 class="a-small">DIY - Cut off jeans</h3>
+	<p class="a-fineprint">22 / 09 / 2015  |  2 kommentarer</p>
+</div>
+
+<div class="m-preview-diy l-gutter">
+	<img src="<?php bloginfo( 'stylesheet_directory' ); ?>/demo_images/diy3.jpg" alt="Monthly Makers rynden t-shirt">
+	<h3 class="a-small">DIY - Marmorerad muggar i BOBO nr3</h3>
+	<p class="a-fineprint">22 / 09 / 2015  |  2 kommentarer</p>
+</div>
+
+<div class="m-preview-diy l-gutter">
+	<img src="<?php bloginfo( 'stylesheet_directory' ); ?>/demo_images/diy4.jpg" alt="Monthly Makers rynden t-shirt">
+	<h3 class="a-small">DIY - Soffbord i guld och marmor</h3>
+	<p class="a-fineprint">22 / 09 / 2015  |  2 kommentarer</p>
+</div>
 <?php get_footer(); ?>
