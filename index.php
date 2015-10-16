f925c40570abee76b86a576dc31bea345d914666
annamarialarsson <annamaria@Annas-MacBook-Pro.local>
2015-10-16 15:22:44 +0200

    Gridview anpassad till alla skärmar

--- index.php
+++ index.php
@@ -1,71 +1,77 @@
 <?php get_header(); ?>
 
-<div class="m-preview-diy l-gutter">
-	<img src="<?php bloginfo( 'stylesheet_directory' ); ?>/demo_images/diy1.jpg" alt="Monthly Makers rynden t-shirt">
-	<h3 class="a-small">DIY - Monthly Makers RYMDEN</h3>
-	<p class="a-fineprint">22 / 09 / 2015  |  2 kommentarer</p>
-</div>
+<div class="o-centered-whitespace">
+	<div class="o-gridview padded l-gutter"> <!-- gridview börjar här -->
+		<div class="m-preview-diy m-gridview__item">
+			<img src="<?php bloginfo( 'stylesheet_directory' ); ?>/demo_images/diy1.jpg" alt="Monthly Makers rynden t-shirt">
+			<h3 class="a-small">DIY - Monthly Makers RYMDEN</h3>
+			<p class="a-fineprint">22 / 09 / 2015  |  2 kommentarer</p>
+		</div>
 
-<div class="m-preview-diy l-gutter">
-	<img src="<?php bloginfo( 'stylesheet_directory' ); ?>/demo_images/diy2.jpg" alt="Monthly Makers rynden t-shirt">
-	<h3 class="a-small">DIY - Cut off jeans</h3>
-	<p class="a-fineprint">22 / 09 / 2015  |  2 kommentarer</p>
-</div>
+		<div class="m-preview-diy m-gridview__item">
+			<img src="<?php bloginfo( 'stylesheet_directory' ); ?>/demo_images/diy2.jpg" alt="Monthly Makers rynden t-shirt">
+			<h3 class="a-small">DIY - Cut off jeans</h3>
+			<p class="a-fineprint">22 / 09 / 2015  |  2 kommentarer</p>
+		</div>
 
-<div class="m-preview-diy l-gutter">
-	<img src="<?php bloginfo( 'stylesheet_directory' ); ?>/demo_images/diy3.jpg" alt="Monthly Makers rynden t-shirt">
-	<h3 class="a-small">DIY - Marmorerad muggar i BOBO nr3</h3>
-	<p class="a-fineprint">22 / 09 / 2015  |  2 kommentarer</p>
-</div>
+		<div class="m-preview-diy m-gridview__item">
+			<img src="<?php bloginfo( 'stylesheet_directory' ); ?>/demo_images/diy3.jpg" alt="Monthly Makers rynden t-shirt">
+			<h3 class="a-small">DIY - Marmorerad muggar i BOBO nr3</h3>
+			<p class="a-fineprint">22 / 09 / 2015  |  2 kommentarer</p>
+		</div>
 
-<div class="m-preview-diy l-gutter">
-	<img src="<?php bloginfo( 'stylesheet_directory' ); ?>/demo_images/diy4.jpg" alt="Monthly Makers rynden t-shirt">
-	<h3 class="a-small">DIY - Soffbord i guld och marmor</h3>
-	<p class="a-fineprint">22 / 09 / 2015  |  2 kommentarer</p>
-</div>
-<div class="m-preview-diy l-gutter">
-	<img src="<?php bloginfo( 'stylesheet_directory' ); ?>/demo_images/diy1.jpg" alt="Monthly Makers rynden t-shirt">
-	<h3 class="a-small">DIY - Monthly Makers RYMDEN</h3>
-	<p class="a-fineprint">22 / 09 / 2015  |  2 kommentarer</p>
-</div>
+		<div class="m-preview-diy m-gridview__item">
+			<img src="<?php bloginfo( 'stylesheet_directory' ); ?>/demo_images/diy4.jpg" alt="Monthly Makers rynden t-shirt">
+			<h3 class="a-small">DIY - Soffbord i guld och marmor</h3>
+			<p class="a-fineprint">22 / 09 / 2015  |  2 kommentarer</p>
+		</div>
+		<div class="m-preview-diy m-gridview__item">
+			<img src="<?php bloginfo( 'stylesheet_directory' ); ?>/demo_images/diy1.jpg" alt="Monthly Makers rynden t-shirt">
+			<h3 class="a-small">DIY - Monthly Makers RYMDEN</h3>
+			<p class="a-fineprint">22 / 09 / 2015  |  2 kommentarer</p>
+		</div>
 
-<div class="m-preview-diy l-gutter">
-	<img src="<?php bloginfo( 'stylesheet_directory' ); ?>/demo_images/diy2.jpg" alt="Monthly Makers rynden t-shirt">
-	<h3 class="a-small">DIY - Cut off jeans</h3>
-	<p class="a-fineprint">22 / 09 / 2015  |  2 kommentarer</p>
-</div>
+		<div class="m-preview-diy m-gridview__item">
+			<img src="<?php bloginfo( 'stylesheet_directory' ); ?>/demo_images/diy2.jpg" alt="Monthly Makers rynden t-shirt">
+			<h3 class="a-small">DIY - Cut off jeans</h3>
+			<p class="a-fineprint">22 / 09 / 2015  |  2 kommentarer</p>
+		</div>
 
-<div class="m-preview-diy l-gutter">
-	<img src="<?php bloginfo( 'stylesheet_directory' ); ?>/demo_images/diy3.jpg" alt="Monthly Makers rynden t-shirt">
-	<h3 class="a-small">DIY - Marmorerad muggar i BOBO nr3</h3>
-	<p class="a-fineprint">22 / 09 / 2015  |  2 kommentarer</p>
-</div>
+		<div class="m-preview-diy m-gridview__item">
+			<img src="<?php bloginfo( 'stylesheet_directory' ); ?>/demo_images/diy3.jpg" alt="Monthly Makers rynden t-shirt">
+			<h3 class="a-small">DIY - Marmorerad muggar i BOBO nr3</h3>
+			<p class="a-fineprint">22 / 09 / 2015  |  2 kommentarer</p>
+		</div>
 
-<div class="m-preview-diy l-gutter">
-	<img src="<?php bloginfo( 'stylesheet_directory' ); ?>/demo_images/diy4.jpg" alt="Monthly Makers rynden t-shirt">
-	<h3 class="a-small">DIY - Soffbord i guld och marmor</h3>
-	<p class="a-fineprint">22 / 09 / 2015  |  2 kommentarer</p>
-</div><div class="m-preview-diy l-gutter">
-	<img src="<?php bloginfo( 'stylesheet_directory' ); ?>/demo_images/diy1.jpg" alt="Monthly Makers rynden t-shirt">
-	<h3 class="a-small">DIY - Monthly Makers RYMDEN</h3>
-	<p class="a-fineprint">22 / 09 / 2015  |  2 kommentarer</p>
-</div>
+		<div class="m-preview-diy m-gridview__item">
+			<img src="<?php bloginfo( 'stylesheet_directory' ); ?>/demo_images/diy4.jpg" alt="Monthly Makers rynden t-shirt">
+			<h3 class="a-small">DIY - Soffbord i guld och marmor</h3>
+			<p class="a-fineprint">22 / 09 / 2015  |  2 kommentarer</p>
+		</div>
 
-<div class="m-preview-diy l-gutter">
-	<img src="<?php bloginfo( 'stylesheet_directory' ); ?>/demo_images/diy2.jpg" alt="Monthly Makers rynden t-shirt">
-	<h3 class="a-small">DIY - Cut off jeans</h3>
-	<p class="a-fineprint">22 / 09 / 2015  |  2 kommentarer</p>
-</div>
+		<div class="m-preview-diy m-gridview__item">
+			<img src="<?php bloginfo( 'stylesheet_directory' ); ?>/demo_images/diy1.jpg" alt="Monthly Makers rynden t-shirt">
+			<h3 class="a-small">DIY - Monthly Makers RYMDEN</h3>
+			<p class="a-fineprint">22 / 09 / 2015  |  2 kommentarer</p>
+		</div>
 
-<div class="m-preview-diy l-gutter">
-	<img src="<?php bloginfo( 'stylesheet_directory' ); ?>/demo_images/diy3.jpg" alt="Monthly Makers rynden t-shirt">
-	<h3 class="a-small">DIY - Marmorerad muggar i BOBO nr3</h3>
-	<p class="a-fineprint">22 / 09 / 2015  |  2 kommentarer</p>
-</div>
+		<div class="m-preview-diy m-gridview__item">
+			<img src="<?php bloginfo( 'stylesheet_directory' ); ?>/demo_images/diy2.jpg" alt="Monthly Makers rynden t-shirt">
+			<h3 class="a-small">DIY - Cut off jeans</h3>
+			<p class="a-fineprint">22 / 09 / 2015  |  2 kommentarer</p>
+		</div>
 
-<div class="m-preview-diy l-gutter">
-	<img src="<?php bloginfo( 'stylesheet_directory' ); ?>/demo_images/diy4.jpg" alt="Monthly Makers rynden t-shirt">
-	<h3 class="a-small">DIY - Soffbord i guld och marmor</h3>
-	<p class="a-fineprint">22 / 09 / 2015  |  2 kommentarer</p>
+		<div class="m-preview-diy m-gridview__item">
+			<img src="<?php bloginfo( 'stylesheet_directory' ); ?>/demo_images/diy3.jpg" alt="Monthly Makers rynden t-shirt">
+			<h3 class="a-small">DIY - Marmorerad muggar i BOBO nr3</h3>
+			<p class="a-fineprint">22 / 09 / 2015  |  2 kommentarer</p>
+		</div>
+
+		<div class="m-preview-diy m-gridview__item">
+			<img src="<?php bloginfo( 'stylesheet_directory' ); ?>/demo_images/diy4.jpg" alt="Monthly Makers rynden t-shirt">
+			<h3 class="a-small">DIY - Soffbord i guld och marmor</h3>
+			<p class="a-fineprint">22 / 09 / 2015  |  2 kommentarer</p>
+		</div>
+	</div> <!--	gridview slutar här -->
 </div>
 <?php get_footer(); ?>
